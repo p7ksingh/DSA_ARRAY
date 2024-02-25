12fe0e399cd5ba3f06c01de98ead0d5ccad9bed1
Pankaj Singh <p7ksingh@gmail.com>
2024-02-25 18:37:41 +0530

    marging

---
+++ MargeArray.java
@@ -0,0 +1,50 @@
+package com.arr.array;
+
+public class MargeArray {
+    public static int[] margeArray(int[] arr1, int[] arr2) {
+        int m = arr1.length;
+        int n = arr2.length;
+
+        int[] arr = new int[m + n];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < m && j < n) {
+            if (arr1[i] <= arr2[j]) {
+                arr[k] = arr1[i];
+                i++;
+                k++;
+            } else {
+                arr[k] = arr2[j];
+                j++;
+                k++;
+            }
+        }
+        while (i < m) {
+            arr[k] = arr1[i];
+            i++;
+            k++;
+        }
+        while (j < n) {
+            arr[k] = arr2[j];
+            j++;
+            k++;
+
+        }
+        return arr;
+
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = { 1, 2, 4, 6, 8 };
+        int[] arr2 = { 3, 5, 7, 9 };
+        int[] arr = margeArray(arr1, arr2);
+        printArray(arr);
+    }
+}
